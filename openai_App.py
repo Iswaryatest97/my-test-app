634da232007db2296e6b89117f622ad2fb4627a3
Iswarya97 <iswarya.selvan@softsquare.biz>
2024-06-11 17:39:32 +0530

    updated code

--- openai_App.py
+++ openai_App.py
@@ -1,19 +1,32 @@
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    dotenv_available = True
+except ImportError:
+    dotenv_available = False
 
+# Use dotenv if it's available
+if dotenv_available:
+    print("dotenv is available")
+    load_dotenv()  # Load environment variables from .env file
+else:
+    os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"]
+    print("dotenv is not available")
+    
 # Set your OpenAI API key
 client = OpenAI(
     # This is the default and can be omitted
-    api_key = os.environ.get("OPENAI_API_KEY"),
+    api_key = os.environ.get("OPENAI_API_KEY") if dotenv_available else st.secrets["OPENAI_API_KEY"]
 )
 
 def getEnvironmentDataByKey(key):
-    return os.environ.get(key)
-
+    if dotenv_available:
+        return os.environ.get(key)
+    else:
+        return st.secrets[key]
+    
 def ask_gpt(prompt, messages):    
     
     if prompt:
