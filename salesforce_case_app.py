50870088aed1bcf83e848eabe756ea55796129a7
Iswarya97 <iswarya.selvan@softsquare.biz>
2024-06-25 13:15:22 +0530

    Added embedded service

--- salesforce_case_app.py
+++ salesforce_case_app.py
@@ -14,19 +14,19 @@ print("consumer_key***"+consumer_key)
 print("consumer_secret***"+consumer_secret)
 print("token_url***"+token_url)
 
-custom_css = """
-<style>
-.embeddedServiceHelpButton .helpButton .uiButton {
-    background-color: #005290;
-    font-family: "Arial", sans-serif;
-}
-.embeddedServiceHelpButton .helpButton .uiButton:focus {
-    outline: 1px solid #005290;
-}
-</style>
-"""
-# Inject the custom CSS into the Streamlit app
-st.markdown(custom_css, unsafe_allow_html=True)
+# custom_css = """
+# <style>
+# .embeddedServiceHelpButton .helpButton .uiButton {
+#     background-color: #005290;
+#     font-family: "Arial", sans-serif;
+# }
+# .embeddedServiceHelpButton .helpButton .uiButton:focus {
+#     outline: 1px solid #005290;
+# }
+# </style>
+# """
+# # Inject the custom CSS into the Streamlit app
+# st.markdown(custom_css, unsafe_allow_html=True)
 
 # # Set page configuration
 # st.set_page_config(
@@ -103,58 +103,26 @@ if submitted:
         st.error('Please fill in all required fields.')
 
 html_code = """
-<script type='text/javascript' src='https://service.force.com/embeddedservice/5.0/esw.min.js'></script>
 <script type='text/javascript'>
-	var initESW = function(gslbBaseURL) {
-		embedded_svc.settings.displayHelpButton = true; //Or false
-		embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
+	function initEmbeddedMessaging() {
+		try {
+			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
 
-		//embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to Chat with an Expert)
-		//embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to Agent Offline)
-
-		//embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
-		//embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the domain for your deployment so that visitors can navigate subdomains during a chat session)
-
-		// Settings for Chat
-		//embedded_svc.settings.directToButtonRouting = function(prechatFormData) {
-			// Dynamically changes the button ID based on what the visitor enters in the pre-chat form.
-			// Returns a valid button ID.
-		//};
-		//embedded_svc.settings.prepopulatedPrechatFields = {}; //Sets the auto-population of pre-chat form fields
-		//embedded_svc.settings.fallbackRouting = []; //An array of button IDs, user IDs, or userId_buttonId
-		//embedded_svc.settings.offlineSupportMinimizedText = '...'; //(Defaults to Contact Us)
-
-		embedded_svc.settings.enabledFeatures = ['LiveAgent'];
-		embedded_svc.settings.entryFeature = 'LiveAgent';
-
-		embedded_svc.init(
-			'https://examtestorg-dev-ed.my.salesforce.com',
-			'https://examtestorg-dev-ed.my.site.com/test',
-			gslbBaseURL,
-			'00D2v000002HFkK',
-			'BotWebSupportGrp',
-			{
-				baseLiveAgentContentURL: 'https://c.la1-c1-it3.salesforceliveagent.com/content',
-				deploymentId: '572GC000000Dk8I',
-				buttonId: '573GC0000009ZAC',
-				baseLiveAgentURL: 'https://d.la1-c1-it3.salesforceliveagent.com/chat',
-				eswLiveAgentDevName: 'BotWebSupportGrp',
-				isOfflineSupportEnabled: false
-			}
-		);
+			embeddedservice_bootstrap.init(
+				'00DHu000003oiq4',
+				'Custom_Message_In_App',
+				'https://so1714738901949.my.site.com/ESWCustomMessageInApp1719136220263',
+				{
+					scrt2URL: 'https://so1714738901949.my.salesforce-scrt.com'
+				}
+			);
+		} catch (err) {
+			console.error('Error loading Embedded Messaging: ', err);
+		}
 	};
-
-	if (!window.embedded_svc) {
-		var s = document.createElement('script');
-		s.setAttribute('src', 'https://examtestorg-dev-ed.my.salesforce.com/embeddedservice/5.0/esw.min.js');
-		s.onload = function() {
-			initESW(null);
-		};
-		document.body.appendChild(s);
-	} else {
-		initESW('https://service.force.com');
-	}
 </script>
+<script type='text/javascript' src='https://so1714738901949.my.site.com/ESWCustomMessageInApp1719136220263/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
+
 """
 
 st.markdown(html_code, unsafe_allow_html=True)
