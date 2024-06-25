169efa315e8fed644ea916018a9e96fdc00c6ed9
Iswarya97 <iswarya.selvan@softsquare.biz>
2024-06-25 15:07:17 +0530

    Added embedded script

--- salesforce_case_app.py
+++ salesforce_case_app.py
@@ -102,6 +102,36 @@ if submitted:
     else:
         st.error('Please fill in all required fields.')
 
-components.iframe("/embeddedchat.html", height=500)
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Streamlit External Script Example</title>
+</head>
+<body>
+    <div id="embeddedMessaging">Loading embedded messaging...</div>
+    <script type='text/javascript'>
+        function initEmbeddedMessaging() {
+            try {
+                embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
 
+                embeddedservice_bootstrap.init(
+                    '00DHu000003oiq4',
+                    'Custom_Message_In_App',
+                    'https://so1714738901949.my.site.com/ESWCustomMessageInApp1719136220263',
+                    {
+                        scrt2URL: 'https://so1714738901949.my.salesforce-scrt.com'
+                    }
+                );
+            } catch (err) {
+                console.error('Error loading Embedded Messaging: ', err);
+            }
+        };
+    </script>
+    <script type='text/javascript' src='https://so1714738901949.my.site.com/ESWCustomMessageInApp1719136220263/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
+</body>
+</html>
+"""
 
+# Use Streamlit to display the HTML content
+st.components.v1.html(html_content, height=600)
